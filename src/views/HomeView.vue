6806edfe6987443034bd99edfac80f2d4fa81440
saad <saadmeddiche2004201@gmail.com>
2023-05-16 14:47:54 +0100

    Make The Background Of Home Page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="Home">
-   
+    <div class="body">
+      <div class="left-side"></div>
+      <div class="right-side"></div>
+    </div>
+    <div class="tape">
+      <p>Notre site web officiel sera en ligne très bientôt!</p>
+      <p>Ouverture du cabinet Février 2023.</p>
+    </div>
   </div>
 </template>
 
@@ -13,5 +20,36 @@ export default {
 </script>
 
 <style lang="scss">
+.Home {
+  display: flex;
+  flex-direction: column;
+  height: 95vh;
+
+  .body {
+    display: flex;
+    flex: 1;
+
+    .left-side {
+    flex: 1;
+    background-image: url("../assets/img/left_side.png");
+    background-size:cover;
+    background-repeat: no-repeat;
+    }
+
+    .right-side {
+      flex: 1;
+      background-image: url("../assets/img/right_side.png");
+      background-size: cover;
+    }
+  }
+
+  .tape{
+    height: 5vh;
+  }
+}
+
+
+
+
 
 </style>
